0b8f25ed5d6f448f931314f43dd2225f984909a5
Johann Aschenloher <jaschenl@hm.edu>
2023-04-07 11:46:36 +0200

    Add some usefull scripts

---
+++ download_weigths.sh
@@ -0,0 +1,2 @@
+wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7.pt
+wget wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7-tiny.pt
---
+++ test.sh
@@ -0,0 +1,1 @@
+python test.py --data /data/sam.yaml --img 224 --batch 32 --conf 0.001 --iou 0.65 --device 0 --weights yolov7.pt --name yolov7_224_val
