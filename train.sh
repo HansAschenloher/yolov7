1b13e78da6d8ad6e3f881d32e2148cc6cf1c146d
Johann Aschenloher <jaschenl@hm.edu>
2023-04-07 13:33:47 +0200

    Add a tiny model for SAM

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 DATA=/data/sam.yaml
 
-python train.py --workers 8 --device 0 --batch-size 32 --data $DATA --img 224 224 --cfg cfg/training/yolov7-tiny-sam.yaml --weights '' --name yolov7 --hyp data/hyp.scratch.p5.yaml
+python train.py --workers 8 --device 0 --batch-size 32 --data $DATA --img 224 224 --cfg cfg/training/yolov7-tiny-sam.yaml --weights ./yolov7-tiny.pt --name yolov7 --hyp data/hyp.scratch.p5.yaml
