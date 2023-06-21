abe94a50ab7a7dacca36a33b52ebcff7faaf986b
Alexander Schreiner <a.schreiner0@hm.edu>
2023-06-21 13:22:23 +0200

    Allow jupyter lab to be accessed from remote

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --MappingKernelManager.cull_idle_timeout=18000 --NotebookApp.allow_origin='*' --NotebookApp.token='' --NotebookApp.password=''
+jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --MappingKernelManager.cull_idle_timeout=18000 --NotebookApp.allow_origin='*' --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.disable_check_xsrf=True
